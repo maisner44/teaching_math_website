c3ce02cd4e2516ee49fa90ed948fca24dc94f654
Yehor Hashkodor <gashkoder55@gmail.com>
2024-11-08 20:51:53 +0200

    [ADD] added file uploading and login form css

--- teaching_math_website/lectures/models.py
+++ teaching_math_website/lectures/models.py
@@ -5,6 +5,7 @@ class Lecture(models.Model):
     title = models.CharField(max_length=75)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    upload = models.FileField(upload_to='uploads/', blank=True, null=True)  # File upload field
     
     def __str__(self):
         return self.title
